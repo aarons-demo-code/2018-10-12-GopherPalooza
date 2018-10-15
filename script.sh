a925cb3d4c5d5ac230240373f77fb245f9808de0
Aaron Schlesinger <aaschles@microsoft.com>
2018-10-15 09:36:12 -0700

    spacing'

--- script.sh
+++ script.sh
@@ -2,21 +2,29 @@
 
 # ALL THE PUNS
 
-# open a "VM"
+# start up a "VM"
 docker run -it -v $PWD:/hugo -w /hugo -it golang:1.11 bash
+
 # where is the GOPATH? we aint in it
 echo $GOPATH
+
 # we don't need git y'all
-rm -rf $(which git)
+rm $(which git)
+
 # yea, it's gone I swear
 git
-# we're gonna talk to our top secret Athens server
+
+# let's talk (to the Athens server)
 export GOPROXY=https://microsoftgoproxy.azurewebsites.net
-# put a jacket on it, because this is a cold cache
+
+# put a jacket on it, it's cold (in the local cache)
 ls -al $GOPATH/pkg/mod
-# get your tools cuz we're buildin'
+
+# get your tools cuz we're buildin' yall
 go build
+
 # now the cache is all cozy
 ls -al $GOPATH/pkg/mod
+
 # IT WORKED!
 ./hugo
